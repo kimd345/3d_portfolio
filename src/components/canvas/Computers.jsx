a5c71adafac734f648fe00e374b981c2e7a77f5c
Dan Kim <kimd345@gmail.com>
2023-10-07 19:02:50 -0400

    Create About and Experience sections placeholder

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,41 +1,65 @@
-import { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Suspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
-import CanvasLoader from "../Loader";
+import CanvasLoader from '../Loader'
 
-const Computers = () => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+const Computers = ({ isMobile }) => {
+	const computer = useGLTF('./desktop_pc/scene.gltf')
 
-  return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
-      <primitive object={computer.scene} />
-    </mesh>
-  );
-};
+	return (
+		<mesh>
+			<hemisphereLight intensity={0.15} groundColor='black' />
+			<pointLight intensity={7} />
+			<spotLight
+				position={[-20, 50, 10]}
+				angle={0.12}
+				penumbra={1}
+				intensity={1}
+				castShadow
+				shadow-mapSize={1024}
+			/>
+			<primitive
+				object={computer.scene}
+				scale={isMobile ? 0.25 : 0.75}
+				position={isMobile ? [0, -1.4, -0.4] : [0, -2.75, -1.5]}
+				rotation={[-0.01, -0.2, -0.1]}
+			/>
+		</mesh>
+	)
+}
 
 const ComputersCanvas = () => {
-  return (
-    <Canvas
-      frameLoop="demand"
-      shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers />
-      </Suspense>
+	const [isMobile, setIsMobile] = useState(false)
 
-      <Preload all />
-    </Canvas>
-  );
-};
+	useEffect(() => {
+		const mediaQuery = window.matchMedia('(max-width: 500px)')
+		setIsMobile(mediaQuery.matches)
+		const handleMediaQueryChange = (event) => setIsMobile(event.matches)
+		mediaQuery.addEventListener('change', handleMediaQueryChange)
+		return () =>
+			mediaQuery.removeEventListener('change', handleMediaQueryChange)
+	}, [])
 
-export default Computers;
+	return (
+		<Canvas
+			frameloop='demand'
+			shadows
+			camera={{ position: [20, 3, 5], fov: 25 }}
+			gl={{ preserveDrawingBuffer: true }}
+		>
+			<Suspense fallback={<CanvasLoader />}>
+				<OrbitControls
+					enableZoom={false}
+					maxPolarAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2}
+				/>
+				<Computers isMobile={isMobile} />
+			</Suspense>
+
+			<Preload all />
+		</Canvas>
+	)
+}
+
+export default ComputersCanvas
